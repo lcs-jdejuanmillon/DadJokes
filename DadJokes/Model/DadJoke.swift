65636682d77252071d9d418c7363ffc08acd516e
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-02-22 11:52:11 -0500

    Defined a structure to hold a joke and we used an instance of the structure to show a joke in the view.

---
+++ DadJokes/Model/DadJoke.swift
@@ -0,0 +1,15 @@
+//
+//  DadJoke.swift
+//  DadJokes
+//
+//  Created by Jacobo de Juan Millon on 2022-02-22.
+//
+
+import Foundation
+
+// The DadJoke structure conforms to the Decodable protocol. This means we want Swift to be able to take a JSON object and 'decode' into an instance of this structure
+struct DadJoke: Decodable {
+    let id: String
+    let joke: String
+    let status: Int
+}
